7803c2e337926a3f461af322c3f2aa6fc266208c
ctoto123 <115080900+ctoto123@users.noreply.github.com>
2022-11-21 15:26:13 +0500

    Add files via upload

--- book.php
+++ book.php
@@ -1,26 +1,64 @@
 <?php
-class Book {
-	public $author; //автор
-	public $ganar; // жанр
-	public $name; // название
-	public $ogl; // оглавление 
-	public $kat; // воз. категория 
-	
-	public function __construct($name, $author, $ganar, $ogl, $kat) {
+class Genre{
+}  
+
+
+
+
+
+class Author extends Genre {
+
+	private $surname; // фамилия
+
+	private $name; // имя
+
+	private $patronymic; // отчество
+
+	public function __construct(string $surname, string $name, string $patronymic)
+	{
+		$this->surname = $surname;
 		$this->name = $name;
-		$this->author = $author;
-		$this->ganar = $ganar;
-		$this->org = $ogl;
-		$this->kat = $kat;
+		$this->patronymic = $patronymic;
 	}
 
+	public function setName()
+	{
+		return $this->surname . " " . $this->name . " " . $this->patronymic; // возращаем полнове имя автора
+	}
 }
-$book = new Book("Война и мир","Пушкин", "комедия", "говно просто", "18+");
 
-echo "Book name: " . $book->name . "\n";
-echo "Автор: " . $book->author . "\n";
-echo "Жанр: " . $book->ganar . "\n";
-echo "Вступление: " . $book->org . "\n";
-echo "Возрастная категория: " . $book->kat . "\n";
-echo "\n";
+
+
+
+
+class Book {
+
+	private $name_book; // название книги
+
+	private $annotate; // аннотация
+
+	private $ageCategory; // воз. категория 
+
+	public function __construct(string $name_book, int $ageCategory, string $annotate)
+	{
+		$this->name_book = $name_book;
+		$this->ageCategory = $ageCategory;
+		$this->annotate = $annotate;
+	}
+
+	public function aboutBook() // полное позращение элементов 
+	{
+		return $this->name_book . '<br/>' . $this->ageCategory . '<br/>' . $this->annotate;
+	}
+}
+
+
+$book = new Book("Ромео и Джульетта", 12, "Трагедия о любви юноши и девушки из двух враждующих веронских родов — Монтекки и Капулетти.");
+$book = new Genre("Трагедия");
+$book = new Author("Шекспир", "Уильям", "_");
+
 var_dump($book);
+
+
+
+
